7975c6bf213f34cb14521b341c3e9550e97ffca1
ntn9995 <ngocnguyen9995@gmail.com>
2020-07-13 20:10:01 -0400

    init commit: .gitignore and sample test files

---
+++ test_files/hello_no_block.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    print("Hello world")
+    print("Hello debug world")  # debug
+    print("Hello skip world")  # skip
+    print("hello slow world")  # slow
+
+    # block: slow
+    print("Whatever so slow") # slow
+    assert 0    # debug
+
+    # block: debug
+    assert __debug__    # skip
+    while True:
+        print("Debug")
+
+    assert 1    # debug
