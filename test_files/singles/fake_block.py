871b07adbdb9a6bac3ae7c7bf00d0c1e9931595d
ntn9995 <ngocnguyen9995@gmail.com>
2020-07-15 05:53:54 -0400

    reorganize test_files, 1 folder for single files, 1 folder for multiples

---
+++ test_files/singles/fake_block.py
@@ -0,0 +1,8 @@
+def some_fn():
+
+    some_str = "this should be commented out"   # debug
+
+    # block: debug
+    # this should not count as a block
+    # because it's already commented
+    # end
