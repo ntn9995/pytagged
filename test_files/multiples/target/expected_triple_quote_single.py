185377281486d1228a918cf5ffc37764752bc430
ntn9995 <ngocnguyen9995@gmail.com>
2020-07-15 18:48:16 -0400

    add triple single quote test files in multiples/

---
+++ test_files/multiples/target/expected_triple_quote_single.py
@@ -0,0 +1,23 @@
+def fn_with_docstring():
+    '''This should not be commented out         # debug
+    we don't do anything in a triple quote      # debug
+    block if the start of that block is the     # debug
+    start of a line.                            # debug
+    '''
+
+    # some_str = "this should be commented out normally"  # debug
+
+    # block: debug
+    # triple_quote_str = '''This however would be commented out
+    # if it's block tagged'''
+    # end
+
+    triple_quote_str = '''But triple quote strings can't be commented   # debug
+    out using inline tags'''
+
+    # block: debug
+    '''This should not be commented out, even if it's block tagged
+    '''
+    # end
+
+    return triple_quote_str
