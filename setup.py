d833bb5922f1740944c50cb02271aaacd2ebb3b1
ntn9995 <ngocnguyen9995@gmail.com>
2020-07-15 23:48:51 -0400

    use setup.cfg following PEP 517

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-import setuptools
+from setuptools import setup
 
 
-setuptools.setup(
-    name="pytagged",
-    version="0.0.1",
-    author="1Karus",
-    description="cli utility to help you comment out tagged python code",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    entry_points={
-        "console_scripts": ["pytag=pytagged.cli:main"]
-    },
-    python_requires='>=3.6',
-)
+setup()
