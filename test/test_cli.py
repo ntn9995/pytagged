1958d46d4be09cd1e5a21a03fe0ea70e210589fb
ntn9995 <ngocnguyen9995@gmail.com>
2020-07-17 23:40:35 -0400

    more cli testing: flag options & return code

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,16 +1,74 @@
 from pathlib import Path
 import subprocess
 
+import pytest
 
+from conftest import path_to_multiples
 from pytagged._utils import print_raw_lines, pretty_print_title
+from pytagged._files_utils import filepaths_from_path
 
 
-def test_cli_singles(cleanup_test_path_singles, src_to_target_params):
+TAG_FLAGS = ["-t", "--tags"]
+VERBOSE_FLAGS = ["-v", "--verbose"]
+PRINTONLY_FLAGS = ["-p", "--printonly"]
+BENCHMARK_FLAGS = ["-b", "--benchmark"]
+
+
+@pytest.fixture(params=TAG_FLAGS)
+def flag_tag(request):
+    return request.param
+
+
+@pytest.fixture(params=VERBOSE_FLAGS)
+def flag_verbose(request):
+    return request.param
+
+
+@pytest.fixture(params=PRINTONLY_FLAGS)
+def flag_printonly(request):
+    return request.param
+
+
+@pytest.fixture(params=BENCHMARK_FLAGS)
+def flag_benchmark(request):
+    return request.param
+
+
+@pytest.mark.parametrize(
+    "tags",
+    [
+        ["debug", "", "other_tag"],
+        ["", "debug", "other_tag"],
+        ["debug", "other_tag", ""],
+        [" ", "debug", "other_tag", "", "   "]
+    ]
+)
+def test_cli_empty_tags(tags, flag_tag):
+    path = path_to_multiples()
+    files = filepaths_from_path(path, is_excluded=lambda x: False)
+
+    # Read in the file contents
+    src_content = []
+    for f in files:
+        with open(f) as fin:
+            src_content.append(fin.read())
+
+    cmd = ["pytag", path, flag_tag, *tags]
+    completed = subprocess.run(cmd)
+    assert completed.returncode == 1
+
+    # Check that nothing was changed
+    for i, f in enumerate(files):
+        with open(f) as fin:
+            assert src_content[i] == fin.read()
+
+
+def test_cli_singles(cleanup_test_path_singles, src_to_target_params, flag_tag):
     src_path = src_to_target_params[0]
     target_path = src_to_target_params[1]
     tags = src_to_target_params[2:]
 
-    cmd = ["pytag", src_path, "-t", *tags]
+    cmd = ["pytag", src_path, flag_tag, *tags]
     print(cmd)
     subprocess.run(cmd, check=True)
 
@@ -30,12 +88,15 @@ def test_cli_singles(cleanup_test_path_singles, src_to_target_params):
     assert src_lines == target_lines
 
 
-def test_cli_singles_verbose(cleanup_test_path_singles, src_to_target_params):
+def test_cli_singles_verbose(cleanup_test_path_singles,
+                             src_to_target_params,
+                             flag_tag,
+                             flag_verbose):
     src_path = src_to_target_params[0]
     target_path = src_to_target_params[1]
     tags = src_to_target_params[2:]
 
-    cmd = ["pytag", src_path, "-t", *tags, "-v"]
+    cmd = ["pytag", src_path, flag_tag, *tags, flag_verbose]
     print(cmd)
     subprocess.run(cmd, check=True)
 
@@ -55,11 +116,13 @@ def test_cli_singles_verbose(cleanup_test_path_singles, src_to_target_params):
     assert src_lines == target_lines
 
 
-def test_cli_multiples(cleanup_test_path_multiples, src_to_target_params_multiples):
+def test_cli_multiples(cleanup_test_path_multiples,
+                       src_to_target_params_multiples,
+                       flag_tag):
 
     src_path, target_path = src_to_target_params_multiples[:2]
     tags = src_to_target_params_multiples[2:]
-    cmd = ["pytag", src_path, "-t", *tags]
+    cmd = ["pytag", src_path, flag_tag, *tags]
 
     print(cmd)
     subprocess.run(cmd, check=True)
@@ -83,11 +146,12 @@ def test_cli_multiples(cleanup_test_path_multiples, src_to_target_params_multipl
 
 
 def test_cli_multiples_verbose(cleanup_test_path_multiples,
-                               src_to_target_params_multiples):
+                               src_to_target_params_multiples,
+                               flag_verbose):
 
     src_path, target_path = src_to_target_params_multiples[:2]
     tags = src_to_target_params_multiples[2:]
-    cmd = ["pytag", src_path, "-t", *tags, "-v"]
+    cmd = ["pytag", src_path, "-t", *tags, flag_verbose]
 
     print(cmd)
     subprocess.run(cmd, check=True)
@@ -108,3 +172,31 @@ def test_cli_multiples_verbose(cleanup_test_path_multiples,
         actual = actual_contents[name]
         expected = expected_contents[f"expected_{name}"]
         assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "tags",
+    [
+        ["debug", "tag", "other_tag"],
+        ["some_tag", "debug", "other_tag"],
+        ["debug", "other_tag", "some_dumb_tag"],
+        ["fan_tag_stick", "debug", "other_tag", "bro_tag", "suhhhhh"]
+    ]
+)
+def test_cli_printonly(tags, flag_printonly):
+    path = path_to_multiples()
+    files = filepaths_from_path(path, is_excluded=lambda x: False)
+
+    # Read in the file contents
+    src_content = []
+    for f in files:
+        with open(f) as fin:
+            src_content.append(fin.read())
+
+    cmd = ["pytag", path, "-t", *tags, flag_printonly]
+    subprocess.run(cmd, check=True)
+
+    # Check that nothing was changed
+    for i, f in enumerate(files):
+        with open(f) as fin:
+            assert src_content[i] == fin.read()
