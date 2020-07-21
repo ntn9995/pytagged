35f9cf04a809de90d26d9f24b27a9d2e9a837349
ntn9995 <ngocnguyen9995@gmail.com>
2020-07-21 04:18:14 -0400

    add tests for running pytag using a config file

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,13 @@
+import configparser
 from pathlib import Path
-from typing import Tuple, Generator
+import os
+from typing import Tuple, Iterator
 from string import Template
+from typing import Mapping, Dict, List
 
 import pytest
+
+from pytagged.app import App
 
 
 TEST_FILES_PATH = "./test_files"
@@ -16,6 +21,7 @@ PARAMS = [("hello.py", "expected_hello.py", "debug"),
           ("fake_block.py", "expected_fake_block.py", "debug"),]
 
 PARAMS_MULTIPLES = [("src", "target", "debug")]
+
 
 src_singles_path_template = Template(
     f"{TEST_FILES_PATH}/singles/src/$filename")
@@ -47,13 +53,87 @@ def path_to_multiples() -> str:
     return f"{TEST_FILES_PATH}/multiples"
 
 
-@pytest.fixture(scope="module", params=PARAMS)
+def read_python_files_as_dict(path: str) -> Dict[str, List[str]]:
+    file_contents = {}
+    for f in Path(path).glob(r"**/*.py"):
+        fname = str(f)
+        with f.open() as fin:
+            file_contents[fname] = fin.readlines()
+    return file_contents
+
+
+def write_file_from_dict(contents: Mapping[str, List[str]]):
+    for filename, lines in contents.items():
+        with open(filename, 'w') as f:
+            f.writelines(lines)
+
+
+@pytest.fixture(scope="module")
+def generate_default_test_config() -> Iterator[str]:
+    """Generate a config file using the default
+    options, replacing the path to the test files
+    path
+    """
+    app = App()
+    default_options = app.default_opts()
+    # set the path to the multiple src
+    test_src_file_path = f"{path_to_multiples()}/src"
+    default_options = default_options._replace(path=test_src_file_path)
+    config_path = app.create_config_file_from_options(default_options)
+
+    yield config_path
+    os.remove(config_path)
+
+
+@pytest.fixture
+def generate_options_mixins(src_to_target_params):
+    """Mix between cli & and cfg opts. Cli provides
+    tags, cfg provides path.
+    """
+    app = App()
+    options = app.default_opts()
+    src_path = src_to_target_params[0]
+    options = options._replace(path=src_path, tags=[])
+    print(options)
+    config_path = app.create_config_file_from_options(options)
+
+    # yield the config path and the parameters
+    yield (config_path, *src_to_target_params)
+
+    # os.remove(config_path)
+
+
+@pytest.fixture(params=PARAMS)
 def src_to_target_params(request) -> Tuple[str, ...]:
 
     src_path = path_to_src_file_singles(request.param[0])
     target_path = path_to_target_file_singles(request.param[1])
 
     return (src_path, target_path, *request.param[2:])
+
+
+@pytest.fixture(params=PARAMS)
+def src_to_target_params_singles_use_config(request) -> Tuple[str, ...]:
+    config = configparser.ConfigParser()
+    src_path = path_to_src_file_singles(request.param[0])
+    target_path = path_to_target_file_singles(request.param[1])
+
+    config_path = f"{TEST_FILES_PATH}/test.ini"
+
+    section = {
+        "path": src_path,
+        "config_path": config_path,
+        "tags": ','.join(request.param[2:])
+    }
+
+    config["pytagged"] = section
+
+    # write config
+    with open(config_path, 'w') as f:
+        config.write(f)
+
+    yield config_path, target_path
+    os.remove(config_path)
 
 
 @pytest.fixture(scope="module", params=PARAMS_MULTIPLES)
@@ -68,8 +148,32 @@ def src_to_target_params_multiples(request) -> Tuple[str, ...]:
     return (src_path, target_path, *tags)
 
 
+@pytest.fixture(params=PARAMS_MULTIPLES)
+def src_to_target_params_multiples_use_config(request) -> Iterator[Tuple[str, ...]]:
+    config = configparser.ConfigParser()
+    src_path = path_to_src_file_singles(request.param[0])
+    target_path = path_to_target_file_singles(request.param[1])
+
+    config_path = f"{TEST_FILES_PATH}/test.ini"
+
+    section = {
+        "path": src_path,
+        "config_path": config_path,
+        "tags": ','.join(request.param[2:])
+    }
+
+    config["pytagged"] = section
+
+    # write config
+    with open(config_path, 'w') as f:
+        config.write(f)
+
+    yield config_path, target_path
+    os.remove(config_path)
+
+
 @pytest.fixture
-def cleanup_test_path_multiples() -> Generator[int, None, None]:
+def cleanup_test_path_multiples() -> Iterator[int]:
     src_path = Path(f"{TEST_FILES_PATH}/multiples/src/")
     src_files = list(src_path.glob("*.py"))
     src_contents = {}
@@ -87,7 +191,7 @@ def cleanup_test_path_multiples() -> Generator[int, None, None]:
 
 
 @pytest.fixture
-def cleanup_test_path_singles() -> Generator[int, None, None]:
+def cleanup_test_path_singles() -> Iterator[int]:
     src_path = Path(f"{TEST_FILES_PATH}/singles/src/")
     src_files = list(src_path.glob("*.py"))
     src_contents = {}
--- test/test_cli.py
+++ test/test_cli.py
@@ -1,17 +1,27 @@
+import configparser
 from pathlib import Path
 import subprocess
+from typing import Sequence, Mapping
 
 import pytest
 
-from conftest import path_to_multiples
+from conftest import (
+    path_to_multiples,
+    read_python_files_as_dict,
+    write_file_from_dict
+)
 from pytagged._utils import print_raw_lines, pretty_print_title
 from pytagged._files_utils import filepaths_from_path
+from pytagged._mode import Mode
 
 
 TAG_FLAGS = ["-t", "--tags"]
-VERBOSE_FLAGS = ["-v", "--verbose"]
+VERBOSE_FLAGS = ["-v", "--verbosity"]
 PRINTONLY_FLAGS = ["-p", "--printonly"]
 BENCHMARK_FLAGS = ["-b", "--benchmark"]
+CONFIG_FLAGS = ["-cf", "--config"]
+EXCLUDE_FLAGS = ["-x", "--exclude"]
+EXTEND_FLAGS = ["-xt", "--extend-exclude"]
 
 
 @pytest.fixture(params=TAG_FLAGS)
@@ -32,6 +42,33 @@ def flag_printonly(request):
 @pytest.fixture(params=BENCHMARK_FLAGS)
 def flag_benchmark(request):
     return request.param
+
+
+@pytest.fixture(params=CONFIG_FLAGS)
+def flag_config(request):
+    return request.param
+
+
+@pytest.fixture(params=EXCLUDE_FLAGS)
+def flag_exclude(request):
+    return request.param
+
+
+@pytest.fixture(params=EXTEND_FLAGS)
+def flag_extend(request):
+    return request.param
+
+
+def read_config(config_path: str) -> Mapping[str, str]:
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+
+def print_rawlines_pretty(name: str, lines: Sequence[str]):
+    pretty_print_title(name)
+    print_raw_lines(lines)
+    print('')
 
 
 @pytest.mark.parametrize(
@@ -78,12 +115,8 @@ def test_cli_singles(cleanup_test_path_singles, src_to_target_params, flag_tag):
     with open(target_path) as f:
         target_lines = f.readlines()
 
-    pretty_print_title(src_path)
-    print_raw_lines(src_lines)
-    print('')
-    pretty_print_title(target_path)
-    print_raw_lines(target_lines)
-    print('')
+    print_rawlines_pretty(src_path, src_lines)
+    print_rawlines_pretty(target_path, target_lines)
 
     assert src_lines == target_lines
 
@@ -96,7 +129,7 @@ def test_cli_singles_verbose(cleanup_test_path_singles,
     target_path = src_to_target_params[1]
     tags = src_to_target_params[2:]
 
-    cmd = ["pytag", src_path, flag_tag, *tags, flag_verbose]
+    cmd = ["pytag", src_path, flag_tag, *tags, flag_verbose, "1"]
     print(cmd)
     subprocess.run(cmd, check=True)
 
@@ -106,12 +139,8 @@ def test_cli_singles_verbose(cleanup_test_path_singles,
     with open(target_path) as f:
         target_lines = f.readlines()
 
-    pretty_print_title(src_path)
-    print_raw_lines(src_lines)
-    print('')
-    pretty_print_title(target_path)
-    print_raw_lines(target_lines)
-    print('')
+    print_rawlines_pretty(src_path, src_lines)
+    print_rawlines_pretty(target_path, target_lines)
 
     assert src_lines == target_lines
 
@@ -151,7 +180,7 @@ def test_cli_multiples_verbose(cleanup_test_path_multiples,
 
     src_path, target_path = src_to_target_params_multiples[:2]
     tags = src_to_target_params_multiples[2:]
-    cmd = ["pytag", src_path, "-t", *tags, flag_verbose]
+    cmd = ["pytag", src_path, "-t", *tags, flag_verbose, "1"]
 
     print(cmd)
     subprocess.run(cmd, check=True)
@@ -200,3 +229,140 @@ def test_cli_printonly(tags, flag_printonly):
     for i, f in enumerate(files):
         with open(f) as fin:
             assert src_content[i] == fin.read()
+
+
+def test_cli_singles_with_opt_from_cfg(cleanup_test_path_singles,
+                                       src_to_target_params_singles_use_config,
+                                       flag_config):
+    config_path, target_path = src_to_target_params_singles_use_config
+
+    cmd = ["pytag", flag_config, config_path]
+    subprocess.run(cmd, check=True)
+
+    config = read_config(config_path)
+    src_path = config["pytagged"]["path"]
+    with open(src_path) as f:
+        src_lines = f.readlines()
+
+    with open(target_path) as f:
+        target_lines = f.readlines()
+
+    print_rawlines_pretty(src_path, src_lines)
+    print_rawlines_pretty(target_path, target_lines)
+    assert src_lines == target_lines
+
+
+def test_cli_multiples_with_opt_from_cfg(cleanup_test_path_multiples,
+                                         src_to_target_params_multiples_use_config,
+                                         flag_config):
+    config_path, target_path = src_to_target_params_multiples_use_config
+
+    cmd = ["pytag", flag_config, config_path]
+    subprocess.run(cmd, check=True)
+    config = read_config(config_path)
+    src_path = config["pytagged"]["path"]
+
+    expected_contents = {}
+    for f in Path(target_path).glob(r"**/*.py"):
+        fname = f.parts[-1]
+        with f.open() as fin:
+            expected_contents[fname] = fin.readlines()
+
+    actual_contents = {}
+    for f in Path(src_path).glob(r"**/*.py"):
+        fname = f.parts[-1]
+        with f.open() as fin:
+            actual_contents[fname] = fin.readlines()
+
+    for name in actual_contents:
+        actual = actual_contents[name]
+        expected = expected_contents[f"expected_{name}"]
+        assert actual == expected
+
+
+def test_cli_no_cli_args(generate_default_test_config,
+                         flag_tag, flag_exclude, flag_extend,
+                         flag_verbose, flag_printonly, flag_benchmark):
+    """This test uses the config file generated.
+    This is meant to test working with a config file works
+    correctly with no cli args. Using options from the config
+    file should be equivalent to using the same options from
+    the cli.
+    """
+    config_path = generate_default_test_config
+    print(config_path)
+    config = read_config(config_path)
+    pytagged_section = config["pytagged"]
+    src_file_path = pytagged_section["path"]
+    og_file_contents = read_python_files_as_dict(src_file_path)
+
+    tags = [i.strip() for i in pytagged_section["tags"].split(',')]
+    exclude = [i.strip() for i in pytagged_section["exclude"].split(',')]
+    extend = [i.strip() for i in pytagged_section["extend_exclude"].split(',')]
+
+    args = [
+        src_file_path,
+        flag_tag, *tags,
+        flag_exclude, *exclude,
+        flag_extend, *extend,
+        flag_verbose, pytagged_section["verbosity"]
+    ]
+    mode = pytagged_section["mode"]
+    if mode is Mode.PRINTONLY:
+        args.append(flag_printonly)
+    elif mode is Mode.BENCHMARK:
+        args.append(flag_benchmark)
+        args.append(config["benchmark_runs"])
+
+    # use pytag with cli args first
+    cmd = ["pytag", *args]
+    subprocess.run(cmd, check=True)
+
+    cli_result = read_python_files_as_dict(src_file_path)
+
+    # restore the original content
+    write_file_from_dict(og_file_contents)
+
+    cmd = ["pytag"]
+    subprocess.run(cmd, check=True)
+
+    cfg_result = read_python_files_as_dict(src_file_path)
+
+    for k in cli_result:
+        cli_lines = cli_result[k]
+        cfg_lines = cfg_result[k]
+        assert cli_lines == cfg_lines
+
+    # restore again
+    write_file_from_dict(og_file_contents)
+
+
+def test_cli_mixins(cleanup_test_path_singles,
+                    generate_options_mixins,
+                    flag_config,
+                    flag_tag):
+    """Runs pytagged with mixed options from both cli
+    and cfg/ini file. The test expects the tags to be
+    provided by the cli, and the path to be provided
+    by the config file. Should be the same result as
+    test_cli_singles
+    """
+
+    config_path = generate_options_mixins[0]
+    src_path = generate_options_mixins[1]
+    target_path = generate_options_mixins[2]
+    tags = generate_options_mixins[3:]
+
+    cmd = ["pytag", flag_config, config_path, flag_tag, *tags]
+    subprocess.run(cmd, check=True)
+
+    with open(src_path) as f:
+        src_lines = f.readlines()
+
+    with open(target_path) as f:
+        target_lines = f.readlines()
+
+    print_rawlines_pretty(src_path, src_lines)
+    print_rawlines_pretty(target_path, target_lines)
+
+    assert src_lines == target_lines
